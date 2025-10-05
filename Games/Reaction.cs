654adea2c1e27e9e49973569e2014e93944d703e
HoChan8253 <nahcohochan@gmail.com>
2025-10-05 18:20:30 +0900

    [fixed] games 4
    fixed reaction

--- Games/Reaction.cs
+++ Games/Reaction.cs
@@ -23,41 +23,64 @@ namespace HoChanMiniGames
             Effects.PrintColor("엔터", ConsoleColor.Yellow);
             Console.WriteLine("를 바로 누르세요!");
             Console.WriteLine(" ( 3초 이내에 누르지 않으면 실패 )\n");
+            Effects.PrintColor("'지금!'", ConsoleColor.Yellow);
+            Console.WriteLine("이 뜨기 전에 Enter를 여러번 누르지마세요. 꼬입니다 ㅠㅠ");
             Console.WriteLine("준비 되셨으면 아무 키나 입력해주세요.");
             Console.ReadKey();
 
             List<double> result = new List<double>(); // 3번 측정 값을 담을 List
             Random rnd = new Random();
-
-            #region 수정 전
-            //Console.WriteLine("준비...");
-            //Thread.Sleep(1000);
-
-            //Random rnd = new Random();
-            //Thread.Sleep(rnd.Next(2000, 8000));
-
-            //Effects.PrintColor("'지금!'\n", ConsoleColor.Yellow);
-            //DateTime start = DateTime.Now;
-            #endregion
-
+                        
             for ( int round = 1; round <= 3; round++ )
             {
-                Console.Clear();
-                Console.WriteLine($"Round {round} / 3");
-                Effects.PrintColor("준비...", ConsoleColor.Red);
-                Thread.Sleep(1000);
+                while (true) // 조기 입력 시 재시도하기 위한 루프 
+                {
+                    Console.Clear();
+                    Console.WriteLine($"Round {round} / 3");
+                    Effects.PrintColor("준비...", ConsoleColor.Red);
+                    Thread.Sleep(1000);
 
-                Thread.Sleep(rnd.Next(2000, 8000));
+                    //Thread.Sleep(rnd.Next(2000, 8000));
+                    int waitTime = rnd.Next(2000, 8000); // 기다리는 시간 값만 저장 ( Thread.Sleep 적용 x )
+                    int elapsed = 0; // 경과 시간
+                    int step = 50; // 50ms 단위로 검사
 
-                Effects.PrintColor("지금!", ConsoleColor.Yellow);
-                DateTime start = DateTime.Now;
+                    bool early = false;
 
-                Console.ReadLine();
-                double ms = (DateTime.Now - start).TotalMilliseconds;
-                result.Add(ms);
+                    while (elapsed < waitTime)
+                    {
+                        if(Console.KeyAvailable) // 키가 눌렸는지 검사
+                        {
+                            ConsoleKeyInfo key = Console.ReadKey(true);
+                            if (key.Key == ConsoleKey.Enter)
+                            {
+                                early = true;
+                                break;
+                            }
+                        }
+                        Thread.Sleep(step);
+                        elapsed += step;
+                    }
 
-                Console.WriteLine($"반응 속도 : {ms:F0}"); // 소수점 아래 표시 안함
-                Thread.Sleep(1500);
+                    if (early)
+                    {
+                        Effects.PrintColor("\n너무 빠르게 입력하셨어요!\n", ConsoleColor.Red);
+                        Console.WriteLine("다시 시도합니다.");
+                        Thread.Sleep(1500);
+                        continue; // while(true) 루프 처음으로 되돌아감
+                    }
+
+                    Effects.PrintColor("지금!", ConsoleColor.Yellow);
+                    DateTime start = DateTime.Now;
+
+                    Console.ReadLine();
+                    double ms = (DateTime.Now - start).TotalMilliseconds;
+                    result.Add(ms);
+
+                    Console.WriteLine($"반응 속도 : {ms:F0}"); // 소수점 아래 표시 안함
+                    Thread.Sleep(1500);
+                    break; // 다음 라운드 진행
+                }
             }
 
             double average = 0;
@@ -89,5 +112,16 @@ namespace HoChanMiniGames
             Console.WriteLine("엔터를 눌러주세요.");
             Console.ReadLine();
         }
+
+        #region 수정
+        //Console.WriteLine("준비...");
+        //Thread.Sleep(1000);
+
+        //Random rnd = new Random();
+        //Thread.Sleep(rnd.Next(2000, 8000));
+
+        //Effects.PrintColor("'지금!'\n", ConsoleColor.Yellow);
+        //DateTime start = DateTime.Now;
+        #endregion
     }
 }
---
+++ System/Utile.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoChanMiniGames
+{
+    public class Utile
+    {
+        // Fisher - Yates shuffle
+        private static void shuffle<T>(List<T> list)
+        {
+            var rnd = new Random();
+
+            for ( int i = list.Count - 1; i > 0; i-- )
+            {
+                int j = rnd.Next(i + 1); // 0 부터 i 까지 랜덤 인덱스 선택
+                
+                // Swap
+                T temp = list[i];
+                list[i] = list[j];
+                list[j] = temp;
+            }
+        }
+
+        /*public static void FisherYatesShuffle(int[] inputArr)
+        {
+            Random random = new Random();
+
+            for (int i = inputArr.Length - 1; i > 0; i--) //인덱스 역순
+            {
+                int j = random.Next(0, i + 1); // 0부터 i까지 랜덤 인덱스 선택
+
+                // 배열 요소 교환
+                int temp = inputArr[i];
+                inputArr[i] = inputArr[j];
+                inputArr[j] = temp;
+            }
+        }*/
+    }
+}
